b0810e373d3b272cf27a208bfa9a111f32049921
Darshanshresthaa <darshanshrestha332@gmail.com>
2026-07-10 22:13:41 +0545

    Updates

---
+++ MCP/Tools/__init__.py
@@ -0,0 +1,4 @@
+from .create import *
+from .Delete import *
+from .Read import *
+from .Pull import *
--- demo.py
+++ demo.py
@@ -1,7 +1,5 @@
-from fastmcp import FastMCP
+from MCP.server import mcp
 from MCP.Tools import *
-
-mcp = FastMCP("ForgeMCP")
 
 if __name__ == "__main__":
     mcp.run()
