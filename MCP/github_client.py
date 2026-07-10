7e23d78b13517282fe86376ea4ffe46f26ec0c89
Darshanshresthaa <darshanshrestha332@gmail.com>
2026-07-10 16:16:41 +0545

    Immplement put post and delete  request

--- MCP/github_client.py
+++ MCP/github_client.py
@@ -53,3 +53,56 @@ def github_put(endpoint: str, json: dict):
 
 
     return response.json()
+
+def github_post(endpoint: str, json: dict):
+    """
+    Send PUT request to GitHub API.
+    """
+
+    url = f"{GITHUB_API}{endpoint}"
+
+
+    headers = {
+        "Authorization": f"Bearer {HEADERS}",
+        "Accept": "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+
+
+    response = requests.post(
+        url,
+        headers=headers,
+        json=json
+    )
+
+
+    if response.status_code not in [200, 201]:
+        raise Exception(
+            f"GitHub API Error {response.status_code}: {response.text}"
+        )
+
+
+    return response.json()
+
+
+def git_delete(endpoint: str):
+
+    url = f"{GITHUB_API_URL}{endpoint}"
+
+    headers = {
+        "Authorization": f"Bearer {GITHUB_TOKEN}",
+        "Accept": "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2022-11-28",
+    }
+
+    response = requests.delete(
+        url,
+        headers=headers,
+    )
+
+    if response.status_code != 204:
+        raise Exception(
+            f"GitHub API Error {response.status_code}: {response.text}"
+        )
+
+    return {"status": "success"}
