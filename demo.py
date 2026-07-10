1cb1a2c5cb8eb914e022e9dffdda6ef648541d33
Darshanshresthaa <darshanshrestha332@gmail.com>
2026-07-10 22:33:50 +0545

    updating demo.py for Remote Hosting

--- demo.py
+++ demo.py
@@ -2,4 +2,5 @@ from MCP.server import mcp
 from MCP.Tools import *
 
 if __name__ == "__main__":
-    mcp.run()
+    port = int(os.environ.get("PORT", 8000))
+    mcp.run(transport="http", host="0.0.0.0", port=port)
