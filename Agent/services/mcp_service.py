f89b261c030f828b973800c118b575703b79129d
Darshanshresthaa <darshanshrestha332@gmail.com>
2026-07-18 19:18:32 +0545

    Implement of Git Mcp Server

---
+++ Agent/services/mcp_service.py
@@ -0,0 +1,10 @@
+
+SERVERS = {
+    "ForgeMCP": {
+        "transport": "streamable_http",
+        "url": "https://gitserver-production.up.railway.app/mcp",
+        "headers": {
+            "Authorization": "Bearer KUm725H1HOhChd0Wkclg1UVQMtqU4w4D16fgzTmQEq8"
+        }
+    }
+}
