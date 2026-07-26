c1a0f00c59e56ee65e1f1034b9302e90f1f9e2ba
Darshanshresthaa <darshanshrestha332@gmail.com>
2026-07-26 15:24:08 +0545

    Refactor LLM and MCP server configuration

---
+++ Agent/service.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+from langchain_mistralai import ChatMistralAI
+
+load_dotenv()
+
+
+def get_llm():
+    model = ChatMistralAI(
+        model="mistral-small-latest",
+        api_key=os.getenv("MISTRAL_API_KEY"),
+        streaming=True,
+    )
+
+    return model
+
+
+def get_mcp_server():
+    SERVERS = {
+        "ForgeMCP": {
+            "transport": "streamable_http",
+            "url": "https://gitserver-production.up.railway.app/mcp",
+            "headers": {
+                "Authorization": f"Bearer {os.getenv('MCP_AUTH_TOKEN')}"
+            },
+        }
+    }
+
+    return SERVERS
